16167ced240a1e715627edbd9b924414a010f9ad
Vazrupe <vazrupe@naver.com>
2017-01-15 20:03:02 +0900

    version update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "SlackConnector",
-    version = "0.0.3",
+    version = "0.0.4",
     author="HyeonGyu Lee",
     author_email="vazrupe@gmail.com",
     description="Slack Rtm Client",
